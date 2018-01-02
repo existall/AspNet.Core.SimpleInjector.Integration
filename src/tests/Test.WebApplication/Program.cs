5905417647b3ecc26c5da738712b392cb9120846
Guy <guy.lud@gmail.com>
2018-01-02 22:27:31 +0200

    test server for testing

---
+++ src/tests/Test.WebApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ExistsForAll.SimpleInjector.AspNetCore.Integration;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SimpleInjector.Lifestyles;
+
+namespace Test.WebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			var host = new WebHostBuilder()
+		        .UseKestrel()
+		        .UseContentRoot(Directory.GetCurrentDirectory())
+		        .UseIISIntegration()
+		        .UseSimpleInjector(o => o.DefaultScopedLifestyle = new AsyncScopedLifestyle())
+		        .UseStartup<Startup>()
+		        .UseApplicationInsights()
+		        .Build();
+
+	        host.Run();
+		}
+    }
+}
